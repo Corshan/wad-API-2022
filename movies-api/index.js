ac5e760fdb39a419de4d2eea110bf75ee50f4f9f
Corey Shanahan <coreyshanahan01@gmail.com>
2022-11-24 11:14:12 +0000

    Topic 10 - Session based Auth

--- movies-api/index.js
+++ movies-api/index.js
@@ -5,6 +5,8 @@ import genresRouter from './api/genres';
 import usersRouter from './api/users';
 import './db';
 import './seedDate';
+import session from 'express-session';
+import authenticate from './authenticate';
 
 dotenv.config();
 
@@ -13,11 +15,18 @@ const app = express();
 // eslint-disable-next-line no-undef
 const port = process.env.PORT;
 
+//session middleware
+app.use(session({
+  secret: 'ilikecake',
+  resave: true,
+  saveUninitialized: true
+}));
+
 app.use(express.json());
 
-app.use('/api/movies', moviesRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/movies', authenticate, moviesRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
