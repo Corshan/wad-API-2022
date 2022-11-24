827701b9d178cfc8949775f40f701d5bb591d639
Corey Shanahan <coreyshanahan01@gmail.com>
2022-11-24 12:19:44 +0000

    Topic - Update Movies Data/Route + Mongo lab Exercises with Atlas

--- movies-api/seedDate/index.js
+++ movies-api/seedDate/index.js
@@ -1,6 +1,10 @@
 import userModel from '../api/users/userModel';
 import users from './users';
 import dotenv from 'dotenv';
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
+import genreModel from '../api/genres/genresModel';
+import genres from './genre.js';
 
 dotenv.config();
 
@@ -16,7 +20,34 @@ async function loadUsers() {
   }
 }
 
+// deletes all movies documents in collection and inserts test data
+export async function loadMovies() {
+  console.log('load movie data');
+  console.log(movies.length);
+  try {
+    await movieModel.deleteMany();
+    await movieModel.collection.insertMany(movies);
+    console.info(`${movies.length} Movies were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load movie Data: ${err}`);
+  }
+}
+
+export async function loadGenres() {
+  console.log('load genre data');
+  console.log(genres.length);
+  try {
+    await genreModel.deleteMany();
+    await genreModel.collection.insertMany(genres);
+    console.info(`${genres.length} Generes were successfully stored.`);
+  } catch (err) {
+    console.error(`failed to Load generes Data: ${err}`);
+  }
+}
+
 // eslint-disable-next-line no-undef
 if (process.env.SEED_DB) {
   loadUsers();
+  loadGenres();//you may not need this line if you skipped the exercises
+  loadMovies();//ADD THIS LINE
 }
